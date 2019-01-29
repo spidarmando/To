3d61514dd0c82ac545d97ad9d282a9968ca104b3
armando <armandoallere@gmail.com>
2019-01-29 15:56:53 +0800

    To Do List 2
    to do list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'To';
+  title = 'To Do List';
+  todos = [
+    {
+      label: 'Send Files',
+      done: false, 
+      priority: 3
+    },
+    {
+      label: 'Pull Github',
+      done: true, 
+      priority: 1
+    },
+    {
+      label: 'Push Github',
+      done: false, 
+      priority: 4
+    },
+    {
+      label: 'Commit to Master',
+      done: false, 
+      priority: 5
+    }
+  ];
+
+  addTodo(newTodoLabel) {
+    var newTodo ={
+      label: newTodoLabel,
+      priority: 1,
+      done: false
+    };
+    this.todos.push(newTodo);
+  }
+
+  deleteTodo(todo) {
+    this.todos = this.todos.filter( t => t.label !== todo.label );
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+ 
 
 @NgModule({
   declarations: [
@@ -10,7 +11,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
