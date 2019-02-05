9ebdb53a8d66b6f91af6e2592d3f241c845ea54a
armando <armandoallere@gmail.com>
2019-02-05 10:54:55 +0800

    added show/hide button
    show-hide button interchanged

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'To Do List';
@@ -42,5 +42,6 @@ export class AppComponent {
   deleteTodo(todo) {
     this.todos = this.todos.filter( t => t.label !== todo.label );
   }
+
 }
 
